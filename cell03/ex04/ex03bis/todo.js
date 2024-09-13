9ca8ba178493c0796845ab271f1ee570bda9052c
witchapon14 <witchapon14@gmail.com>
2024-09-13 16:39:37 +0700

    c

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,48 +1,65 @@
 $(document).ready(function() {
-    const $ftList = $('#ft_list');
+    function loadTodos() {
+        const todos = getCookie('todos');
+        if (todos) {
+            const todoArray = JSON.parse(todos);
+            todoArray.forEach(todo => addTodoToDOM(todo));
+        }
+    }
+    function saveTodos() {
+        const todoItems = [];
+        $('#ft_list .todo-item').each(function() {
+            todoItems.push($(this).text());
+        });
+        setCookie('todos', JSON.stringify(todoItems), 7);
+    }
 
-    // Load saved todos from cookies
-    loadTodos();
+    // Add a new to-do item to the DOM
+    function addTodoToDOM(todoText) {
+        if (todoText) {
+            const todoItem = $('<div>').addClass('todo-item').text(todoText);
+            $('#ft_list').prepend(todoItem);
+        }
+    }
 
-    // Handle new todo creation
-    $('#New').click(function() {
-        const todo = prompt('Create a new TO DO:');
-        if (todo && todo.trim() !== '') {
-            addTodo(todo);
+    // Set a cookie with a name, value, and expiration days
+    function setCookie(name, value, days) {
+        let expires = '';
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = `expires=${date.toUTCString()};`;
+        }
+        document.cookie = `${name}=${value};${expires}path=/`;
+    }
+
+    // Get a cookie value by name
+    function getCookie(name) {
+        const nameEQ = `${name}=`;
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
+    // Handle click event for 'New' button
+    $('#new-btn').click(function() {
+        const newTodo = prompt('Enter a new to-do:');
+        if (newTodo && newTodo.trim()) {
+            addTodoToDOM(newTodo.trim());
             saveTodos();
         }
     });
 
-    // Add a new todo element
-    function addTodo(todo) {
-        const $todoDiv = $('<div class="item"></div>').text(todo);
-        // Add click event to remove todo
-        $todoDiv.click(function() {
-            if (confirm('Do you want to remove this TO DO?')) {
-                $todoDiv.remove();
-                saveTodos();
-            }
-        });
-        // Add the todo to the top of the list
-        $ftList.prepend($todoDiv);
-    }
-
-    // Save todos to cookies
-    function saveTodos() {
-        const todos = $ftList.children().map(function() {
-            return $(this).text();
-        }).get();
-        const expires = new Date();
-        expires.setTime(expires.getTime() + (7 * 24 * 60 * 60 * 1000)); // คุกกี้หมดอายุใน 7 วัน
-        document.cookie = `todos=${JSON.stringify(todos)}; expires=${expires.toUTCString()}; path=/; SameSite=Lax;`;
-    }
-
-    // Load todos from cookies
-    function loadTodos() {
-        const cookie = document.cookie.split('; ').find(c => c.startsWith('todos='));
-        if (cookie) {
-            const todos = JSON.parse(cookie.split('=')[1]);
-            todos.forEach(addTodo);
+    $('#ft_list').on('click', '.todo-item', function() {
+        const todoText = $(this).text();
+        if (confirm(`Are you sure you want to remove "${todoText}"?`)) {
+            $(this).remove();
+            saveTodos();
         }
-    }
+    });
+    loadTodos();
 });
